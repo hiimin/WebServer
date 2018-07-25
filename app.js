d9a67fe505cd2d941b70b49a6c58b38054256228
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-07-25 14:57:51 +0900

    Make simple chat #3
    merge login

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 
@@ -22,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login',loginRouter);
+app.use('/signup',signupRouter);
+app.use('/index',indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/signup.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+//var connection = require('../public/javascripts/dbconnection');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('signup', { title: 'Signup' });
+});
+
+/*router.post('/',function (req,res,next) {
+    var name = req.body.name;
+    var nickname = req.body.nickname;
+    var id = req.body.id;
+    var password = req.body.password;
+    var confirmPW = req.body.confirmPW;
+
+    var sql = 'INSERT INTO users (id, password, name, nickname) VALUES(?, ? ,? ,?)';
+    var parms = [id,password,name,nickname];
+    if(password!=confirmPW){
+        console.log('비밀 번호를 다시 입력하시오');
+        res.redirect('/signup');
+    } else {
+        connection.query(sql, parms, function (err, rows, fields) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(rows.insertId);
+                res.redirect('/login');
+            }
+        });
+    }
+});*/
+
+
+module.exports = router;
