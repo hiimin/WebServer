82e39a8703b35b18fbfdd621a7e5fcb535d0a1be
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-07-26 17:21:57 +0900

    Save user information in db when sign up

--- routes/signup.js
+++ routes/signup.js
@@ -1,14 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
 //var connection = require('../public/javascripts/dbconnection');
+
+var user = mongoose.Schema({
+    name : String,
+    id : String,
+    password : String,
+    address : String,
+    phoneNumber : String,
+    emergencyInfo : String,
+    gender : String,
+    disabilityType : String,
+    disabilityGrade : String
+});
+
+var model = mongoose.model('user',user);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('signup', { title: 'Signup' });
 });
 
-/*router.post('/',function (req,res,next) {
-    var name = req.body.name;
+router.post('/',function (req,res,next) {
+    console.log(req.body);
+    model.find({id:req.body.id},function(err,data){
+        console.log('State==> data : '+data);
+        if(data.length==0){
+            var newUser = new model({
+                name: req.body.name,
+                id: req.body.id,
+                password: req.body.password,
+                address : req.body.address,
+                phoneNumber : req.body.phoneNumber,
+                emergencyInfo : req.body.emergencyInfo,
+                gender : req.body.gender,
+                disabilityType : req.body.disabilityType,
+                disabilityGrade : req.body.disabilityGrade
+            });
+            newUser.save(function (err,newUser) {
+                if(err) return console.error(err);
+                console.dir(newUser);
+            });
+        }
+        else{
+            console.log('State==> id is already exist.');
+        }
+    });
+    res.redirect('/');
+   /* var name = req.body.name;
     var nickname = req.body.nickname;
     var id = req.body.id;
     var password = req.body.password;
@@ -28,8 +68,8 @@ router.get('/', (req, res, next) => {
                 res.redirect('/login');
             }
         });
-    }
-});*/
+    }*/
+});
 
 
 module.exports = router;
